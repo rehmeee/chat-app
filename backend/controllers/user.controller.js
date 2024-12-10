09061ab12092b7afe7e866ba0cbacd4940fe6433
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-12-10 17:08:58 +0500

    Add user registration controller with error handling

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,22 @@
+import { User } from "../models/user.model.js";
+import { ApiErrors } from "../utils/apiErrors.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+        const {username, email, fullName, password} = req.body;
+        if(!username || !email || !password){
+            throw new ApiErrors(200, "please provide email password and username correctly")
+        }
+       const user =  await User.create({
+            username, 
+            email, 
+            fullName, 
+            password
+        })
+        if (!user) {
+            throw new ApiErrors(200, "no user is created")
+        }
+        return res
+        .statusCode(400)
+        .json(user)
+})
