74c50381db02ab8672ea92c39f38d7286e71942e
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-12-10 17:10:14 +0500

    Implement socket.io integration for real-time messaging in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+import {io} from "socket.io-client"
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const socket = io("http://localhost:5000")
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  useEffect(()=>{
+      socket.emit("chat message", "hello mani boss");
+      socket.on("chat message", (msg)=>{
+        console.log(msg)
+      });
+      socket.on("dissconnect", ()=>{
+        console.log("a user is dissconnect")
+      })
+  },[])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
     </>
   )
 }
