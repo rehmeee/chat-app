e6e7233764ffd4d396436ed0df387611e3e35773
ScriptSorcererMani <chrehmanali5@gmail.com>
2024-12-10 17:09:29 +0500

    Add database connection utility with error handling

---
+++ backend/db/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+import { ApiErrors } from "../utils/apiErrors.js";
+
+const dbConnection = async()=>{
+    try {
+        console.log("connecting!")
+       const response = await mongoose.connect(process.env.DATABASE_URI);
+       console.log(response.connection.host)
+    } catch (error) {
+        throw new ApiErrors(200, "error while connecting to database")
+    }
+}
+
+export {dbConnection}
